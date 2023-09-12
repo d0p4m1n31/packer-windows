89e4808d46373527b3ae477bf4b73e6f861eb2a6
D0p4m1n3 <d0p4m1n3mail@protonmail.com>
2023-09-12 14:37:14 +0200

    Initial build

---
+++ scripts/choco-apps.bat
@@ -0,0 +1,7 @@
+:: Ensure C:\Chocolatey\bin is on the path
+set /p PATH=%PATH%;C:\ProgramData\chocolatey\
+echo %PATH%
+
+:: Install all the things; for example:
+choco install /y 7zip
+choco install /y notepadplusplus
